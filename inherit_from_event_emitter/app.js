24646d6a9bbb2ec484f483899fe5a0f4ae43d704
dannycrews <dancrews@me.com>
2016-06-15 18:32:54 -0400

    Demo util.inherit()

---
+++ inherit_from_event_emitter/app.js
@@ -0,0 +1,27 @@
+var EventEmitter = require('events');
+var util = require('util');
+
+// a function constructor
+function Greetr() {
+  this.greeting = 'Hello world!';
+}
+
+// using inherits from util module
+// any objects created on Greetr should also have
+// access to the methods and properties on the prototype 
+// event emitter 
+util.inherits(Greetr, EventEmitter);
+
+// can still add own methods and properties to Greetr
+Greetr.prototype.greet = function(data) {
+  console.log(this.greeting + ': ' + data);
+  this.emit('greet', data); // because Greetr has access to 'events'
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+  console.log('Someone greeted!: ' + data);
+});
+
+greeter1.greet('Dan');
